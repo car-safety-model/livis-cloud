182e649ea66b939444a33b5679efe33dc3a2786a
ArjunKSoni <aksoni0520@gmail.com>
2024-11-17 18:30:32 +0530

    .

--- app/utils/image_processing.py
+++ app/utils/image_processing.py
@@ -2,7 +2,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
 import os
 from dotenv import load_dotenv
-import streamlit as st
 
 load_dotenv()
 
